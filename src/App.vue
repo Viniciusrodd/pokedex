2caeb758ccfdfc94ac1e51a8cfb72c9f67b0e17e
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-11-13 15:31:43 -0300

    Consumindo a API do pokeapi, e renderizando com v-for

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
   <div id="app">
     <h1>Meu projeto vue</h1>
+    <ul>
+      <li v-for="(pokemon, index) in pokemonVar" :key="pokemon.id">
+        {{ index + 1 }} - {{ pokemon.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      pokemonVar: []
+    }
+  },
+  created(){
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+      .then(response =>{      
+        console.log(`Pegou a lista de pokemons ${this.pokemonVar}`)
+        this.pokemonVar = response.data.results; 
+      })
+      .catch(error => console.log(error))
+  }
 }
 </script>
 
