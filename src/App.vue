fe5b091a622aab4e5b5763f5f3bb340d604a9895
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-11-18 09:37:22 -0300

    Adicionando imagens + favicon

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,55 @@
 <template>
-    <div id="app">
-        <h1 class="title is-1">Welcome to my Pokedex</h1>
+     <div id="app">
         <div class="column is-half is-offset-one-quarter">
-        <div v-for="(pokemon, index) in pokemonVar" :key="pokemon.id">
-            <!--transformando primeira letra do pokemon em maiucula: -->
-            <Pokemon
-            :nome="pokemon.name"
-            :url="pokemon.url" :num="index + 1"
-            />
+            <img class="image is-128x128" src="./assets/pokebola.png">
+            <h1 class="title is-1">Welcome to my Pokedex</h1>
+            <hr>
+            <div v-for="(pokemon, index) in pokemonVar" :key="pokemon.id">
+                <!--transformando primeira letra do pokemon em maiucula: -->
+                <Pokemon
+                    :nome="pokemon.name"
+                    :url="pokemon.url"
+                    :num="index + 1"
+                />
+            </div>
         </div>
-        </div>
-    </div>
+     </div>
 </template>
 
 <script>
-import axios from 'axios';
-import Pokemon from './components/Pokemon';
+import axios from "axios";
+import Pokemon from "./components/Pokemon";
 
 export default {
-  components: {
-    Pokemon
-  },
-  
-  name: 'App',
+     components: {
+          Pokemon,
+     },
 
-  data() {
-    return {
-      pokemonVar: []
-    }
-  },
+     name: "App",
 
-  created(){
-    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
-      .then(response =>{      
-        console.log(`Pegou a lista de pokemons ${this.pokemonVar}`)
-        this.pokemonVar = response.data.results; 
-      })
-      .catch(error => console.log(error))
-  }
-}
+     data() {
+          return {
+               pokemonVar: [],
+          };
+     },
+
+     created() {
+          axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+               .then((response) => {
+                    console.log(`Pegou a lista de pokemons ${this.pokemonVar}`);
+                    this.pokemonVar = response.data.results;
+               })
+               .catch((error) => console.log(error));
+     },
+};
 </script>
-
-
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  margin-top: 60px;
+     font-family: Avenir, Helvetica, Arial, sans-serif;
+     -webkit-font-smoothing: antialiased;
+     -moz-osx-font-smoothing: grayscale;
+     text-align: center;
+     margin-top: 60px;
 }
 </style>
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,110 +1,118 @@
-
-
 <template>
-  <div id="pokemon">
-    <div class="card">
-        <div class="card-image">
-            <figure>
-            <img
-                :src="currentImg"
-                alt="Placeholder image"
-            />
-            </figure>
-        </div>
-      <div class="card-content">
-        <div class="media">
-          <div class="media-content">
-            <p class="title is-4">{{ num }}. {{ upper(nome) }}</p>
-            <p class="subtitle is-6">{{ pokemon.typePoke }}</p>
-            <hr>
-            <button ref="bttChange" class="button is-info is-dark" @click="changeSprit">
-                Change to back view
-            </button>
-          </div>
-        </div>
+     <div id="pokemon">
+          <div class="card">
+               <div class="card-image">
+                    <figure>
+                         <img :src="currentImg" alt="Placeholder image" />
+                    </figure>
+               </div>
+               <div class="card-content">
+                    <div class="media">
+                         <div class="media-content">
+                              <p class="title is-4">
+                                   {{ num }}. {{ upper(nome) }}
+                              </p>
+                              <p class="subtitle is-6">
+                                   {{ pokemon.typePoke }}
+                              </p>
+                              <hr />
+                              <button
+                                   ref="bttChange"
+                                   class="button is-info is-dark"
+                                   @click="changeSprit"
+                              >
+                                   Change to back view
+                              </button>
+                         </div>
+                    </div>
 
-        <div class="content">
-            
-        </div>
-      </div>
-    </div>
-  </div>
+                    <div class="content"></div>
+               </div>
+          </div>
+     </div>
 </template>
 
-
-
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    created(){
-      axios.get(this.url)
-        .then((res) =>{
-          //Criando var type pra receber tipo de pokemon:
-          this.pokemon.typePoke = res.data.types[0].type.name;
-          //Pegando imagem de pokemon, frente/costas:
-          this.pokemon.frontPoke = res.data.sprites.front_default;          
-          this.pokemon.backPoke = res.data.sprites.back_default;
-          this.currentImg = this.pokemon.frontPoke
-          console.log(this.pokemon)
-        })
-    },
+     created() {
+          axios.get(this.url).then((res) => {
+               //Criando var type pra receber tipo de pokemon:
+               this.pokemon.typePoke = res.data.types[0].type.name;
+               //Pegando imagem de pokemon, frente/costas:
+               this.pokemon.frontPoke = res.data.sprites.front_default;
+               this.pokemon.backPoke = res.data.sprites.back_default;
+               this.currentImg = this.pokemon.frontPoke;
+               console.log(this.pokemon);
+          });
+     },
 
-    data(){
-      return { 
-        isFront: true,
-        currentImg: '',
+     data() {
+          return {
+               isFront: true,
+               currentImg: "",
 
-        pokemon: {
-          typePoke: '',   // Inicializando com uma propriedade vazia
-          frontPoke: '',  // Inicializando com uma propriedade vazia
-          backPoke: ''
-        }
-      }
-    },
+               pokemon: {
+                    typePoke: "", // Inicializando com uma propriedade vazia
+                    frontPoke: "", // Inicializando com uma propriedade vazia
+                    backPoke: "",
+               },
+          };
+     },
 
-    props: {
-      num: Number,
-      nome: String,
-      url: String
-    },
+     props: {
+          num: Number,
+          nome: String,
+          url: String,
+     },
 
-    methods: {
-      //transforma 1 letra de string em maiúscula:
-        upper(value){
-            if(!value){
-                return ''
-            }else{          
-                return value[0].toUpperCase() + value.slice(1)
-            }
-        },
+     methods: {
+          //transforma 1 letra de string em maiúscula:
+          upper(value) {
+               if (!value) {
+                    return "";
+               } else {
+                    return value[0].toUpperCase() + value.slice(1);
+               }
+          },
 
-        changeSprit(){
-            if(this.isFront){
-                this.isFront = false
-                this.currentImg = this.pokemon.backPoke
-                this.$refs.bttChange.classList.add('button', 'is-danger', 'is-dark')
-                this.$refs.bttChange.innerText = 'Return to front view'
-            }else{
-                this.isFront = true
-                this.currentImg = this.pokemon.frontPoke
-                this.$refs.bttChange.classList.remove('button', 'is-danger', 'is-dark')
-                this.$refs.bttChange.classList.add('button', 'is-info', 'is-dark')
-                this.$refs.bttChange.innerText = 'Change to back view'
-            }
-        }
-    }
-}
+          changeSprit() {
+               if (this.isFront) {
+                    this.isFront = false;
+                    this.currentImg = this.pokemon.backPoke;
+                    this.$refs.bttChange.classList.add(
+                         "button",
+                         "is-danger",
+                         "is-dark"
+                    );
+                    this.$refs.bttChange.innerText = "Return to front view";
+               } else {
+                    this.isFront = true;
+                    this.currentImg = this.pokemon.frontPoke;
+                    this.$refs.bttChange.classList.remove(
+                         "button",
+                         "is-danger",
+                         "is-dark"
+                    );
+                    this.$refs.bttChange.classList.add(
+                         "button",
+                         "is-info",
+                         "is-dark"
+                    );
+                    this.$refs.bttChange.innerText = "Change to back view";
+               }
+          },
+     },
+};
 </script>
 
-
-
 <style>
-    img{
-        height: 30%;
-        width: 30%;
-    }
-    #pokemon{
-        margin: 40px 0px 40px 0px;
-    }
+img {
+     height: 30%;
+     width: 30%;
+}
+#pokemon {
+     margin: 40px 0px 40px 0px;
+}
 </style>
